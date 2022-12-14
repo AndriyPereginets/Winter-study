a9c3036468858509b820bfa0564f0e7880e90484
Andriy <pereginets1995@gmail.com>
2022-12-14 19:52:52 +0200

    14/12/2022/

--- script.js
+++ script.js
@@ -13,5 +13,43 @@ console.log(arr[1]);
 const America = ['South America', 'North America', 'Mexico'];
 console.log(America[2]);  */
 
-console.log('arr' + " - object");
-console.log(4 + 5);    
+/* console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+/* incr++;
+decr--; */
+
+/* console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8); */
+ 
+/* && - І
+|| - або */
+
+/* let numberOfFilms = +prompt ('Скільки фільмів ви вже переглянули','');
+
+ const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+
+ const a = prompt('Один з останнії переглянутих фільмів?',''),
+    b = prompt('На скільки оцінюєте його',''),
+    c = prompt('Один з останнії переглянутих фільмів?',''),
+    d = prompt('На скільки оцінюєте його','');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB); */
+
+
