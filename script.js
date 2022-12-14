06d1ffb268597640d543f904c91b95a60b125d1b
Andriy <pereginets1995@gmail.com>
2022-12-14 19:20:51 +0200

    firs try

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* const obj = {
+    name: "Jonh",
+    age:25,
+    isMarried: false
+};
+console.log(obj.age);
+
+let arr = ['plum.png', 6, [],];
+console.log(arr[1]);
+
+const America = ['South America', 'North America', 'Mexico'];
+console.log(America[2]);  */
+
+console.log('arr' + " - object");
+console.log(4 + 5);    
