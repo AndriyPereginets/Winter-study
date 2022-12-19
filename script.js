45118c870c5cc091470bae99fb9f30d71487aef9
Andriy <pereginets1995@gmail.com>
2022-12-19 02:40:03 +0200

    cycle

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ console.log(2*4 == 8); */
 /* && - І
 || - або */
 
-/* let numberOfFilms = +prompt ('Скільки фільмів ви вже переглянули','');
+let numberOfFilms = +prompt ('Скільки фільмів ви вже переглянули','');
 
  const personalMovieDB = {
     count: numberOfFilms,
@@ -42,14 +42,163 @@ console.log(2*4 == 8); */
     privat: false
  };
 
- const a = prompt('Один з останнії переглянутих фільмів?',''),
-    b = prompt('На скільки оцінюєте його',''),
-    c = prompt('Один з останнії переглянутих фільмів?',''),
-    d = prompt('На скільки оцінюєте його','');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один з останнії переглянутих фільмів?',''),
+              b = prompt('На скільки оцінюєте його','');
 
-    console.log(personalMovieDB); */
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;  
+            console.log('done'); 
+        } else {
+            console.log('error');
+            i--;
+        }                 
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log("Переглянуто мало фільмів!");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Класичний переглядач");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Ви єбаний кіноман!!!!");
+    } else {
+        console.log("Помилка");
+    }
 
 
+    console.log(personalMovieDB);
+
+
+
+   let num = 50;
+
+  /*  while (num <= 55) {
+        console.log(num);
+        num++;
+   } */
+
+/* do {
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i ===6) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        continue;
+    }
+    console.log(i);
+} */
+
+/* for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+} */
+
+/* let result = '';
+const leghth = 7;
+
+for (let i = 1; i < leghth; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+ console.log(result);
+
+
+ first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+            for (let k = 0; k < 3; k++) {
+                if (k === 2) break first;
+                 console.log(`Third level: ${k}`);
+            }
+        }
+    }
+
+
+    for (let i = 5; i <=10; i++) {
+    console.log(i);
+    }
+
+    for (let i = 20; i >= 10; i--) {
+        if (i === 13) {
+            break;}
+        console.log(i);
+    }
+
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 1) {
+            continue;
+        } 
+             console.log(i);
+        }
+       
+
+        for (let i = 2; i <= 10; i++) {
+            if (i ===3) {
+                continue;
+            }
+            if (i ===5) {
+                continue;
+            }
+            if (i ===7) {
+                continue;
+            }
+            if (i ===9) {
+                continue;
+            }
+                 console.log(i);
+            }
+
+
+
+
+    
+
+    for (let i = 2; i <= 16; i++) {
+        if (i % 2 === 0) {
+            continue;
+        } else {
+            console.log(i);
+        }
+    }
+
+
+    let i = 2;
+    while (i < 16) {
+            i++;
+        if (i % 2 === 0) {
+            continue;
+        } 
+       console.log(i);      
+    }
+   
+
+    const Numbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        Numbers[i - 5] = i;
+    }
+
+    console.log(Numbers);
+    return(Numbers); */
+
+
+    let i = [1, 2, 3, 4,];
+    for (let a of i) {
+        console.log(a);
+    } 
