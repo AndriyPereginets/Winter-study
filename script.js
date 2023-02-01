bef191ecdd0a094d49df3c7225ca7cde85e87416
AndriyPereginets <115173459+AndriyPereginets@users.noreply.github.com>
2023-02-01 11:13:10 +0200

    Elements on page and interaction with them

--- script.js
+++ script.js
@@ -1,362 +1,69 @@
-"use strict";
+'use strict';
 
-/* const obj = {
-    name: "Jonh",
-    age:25,
-    isMarried: false
-};
-console.log(obj.age);
+/* const box = document.getElementById('box');
 
-let arr = ['plum.png', 6, [],];
-console.log(arr[1]);
+console.log(box);
 
-const America = ['South America', 'North America', 'Mexico'];
-console.log(America[2]);  */
+const buttons = document.getElementsByTagName('button')[1];
 
-/* console.log('arr' + " - object");
-console.log(4 + "5");
+console.log(buttons);
 
-let incr = 10,
-    decr = 10;
+const circles = document.getElementsByClassName('circle');
+console.log(circles [2]);
+ // Якщо не звертатись до елементу в доку буде масив, з яким хуй шо зробиш!
 
-/* incr++;
-decr--; */
+const hearts = document.querySelectorAll('.heart');
 
-/* console.log(++incr);
-console.log(--decr);
+hearts.forEach(item => {
+    console.log(item);
+});
 
-console.log(5%2);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart); */
 
-console.log(2*4 == 8); */
- 
-/* && - І
-|| - або */
+const   box = document.getElementById('box'),
+        buttons = document.getElementsByTagName('button'),
+        circles = document.getElementsByClassName('circle'),
+        hearts = document.querySelectorAll('.heart'),
+        oneHeart = document.querySelector('.heart'),
+        wrapper = document.querySelector('wrapper');
 
-/* let numberOfFilms = +prompt ('Скільки фільмів ви вже переглянули','');
+/* box.style.backgroundColor = 'blue';
+box.style.width = '500px'; */
 
- const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
- };
+box.style.cssText = 'background-color: blue; width: 500px';
 
+buttons[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один з останнії переглянутих фільмів?',''),
-              b = prompt('На скільки оцінюєте його','');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;  
-            console.log('done'); 
-        } else {
-            console.log('error');
-            i--;
-        }                 
-    }
-
-    if (personalMovieDB.count < 10) {
-        console.log("Переглянуто мало фільмів!");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Класичний переглядач");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Ви єбаний кіноман!!!!");
-    } else {
-        console.log("Помилка");
-    }
-
-
-    console.log(personalMovieDB); */
-
-
-
-  /*  let num = 50; */
-
-  /*  while (num <= 55) {
-        console.log(num);
-        num++;
-   } */
-
-/* do {
-    console.log(num);
-    num++;
-}
-while (num <= 55);
-
-for (let i = 1; i < 10; i++) {
-    if (i ===6) {
-        break;
-    }
-    console.log(i);
-}
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        continue;
-    }
-    console.log(i);
+/* for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = 'green';
 } */
 
-/* for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
-    }
-} */
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
 
-/* let result = '';
-const leghth = 7;
+const div = document.createElement('div');
+/* const text = document.createTextNode('Im here!'); */
+div.classList.add('black');
 
-for (let i = 1; i < leghth; i++) {
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
+ document.body.append(div);
 
-    result += '\n';
-}
- console.log(result);
+/* document.querySelector('wrapper').append(div); */
 
+/* wrapper.prepend(div); */
 
- first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-            for (let k = 0; k < 3; k++) {
-                if (k === 2) break first;
-                 console.log(`Third level: ${k}`);
-            }
-        }
-    }
+/* hearts[0].before(div); */
+/* hearts[0].after(div); */
 
+/* circles[0].remove(); */
 
-    /* for (let i = 5; i <=10; i++) {
-    console.log(i);
-    } */
-/* 
-    for (let i = 20; i >= 10; i--) {
-        if (i === 13) {
-            break;}
-        console.log(i);
-    }
+/* hearts[0].replaceWith(circles[0]); */
 
-    for (let i = 2; i <= 10; i++) {
-        if (i % 2 === 1) {
-            continue;
-        } 
-             console.log(i);
-        }
-       
+div.innerHTML = '<h1>Hello World!</h1>';
 
-        for (let i = 2; i <= 10; i++) {
-            if (i ===3) {
-                continue;
-            }
-            if (i ===5) {
-                continue;
-            }
-            if (i ===7) {
-                continue;
-            }
-            if (i ===9) {
-                continue;
-            }
-                 console.log(i);
-            }
- */
+/* div.textContent = 'Hello'; */
 
-
-
-    
-
- /*    for (let i = 2; i <= 16; i++) {
-        if (i % 2 === 0) {
-            continue;
-        } else {
-            console.log(i);
-        }
-    } */
-
-
-  /*   let i = 2;
-    while (i < 16) {
-            i++;
-        if (i % 2 === 0) {
-            continue;
-        } 
-       console.log(i);      
-    } */
-   
-
-  /*   const Numbers = [];
-
-    for (let i = 5; i < 11; i++) {
-        Numbers[i - 5] = i;
-    }
-
-    console.log(Numbers);
-    return(Numbers);
-
-
-    let i = [1, 2, 3, 4,];
-    for (let a of i) {
-        console.log(a);
-    } 
-
- let num = 20;
- function showFirstMessage(text) {
-    console.log(text);
-    num = 10;
-    }
- 
- showFirstMessage("Hello kitty!");
- console.log(num);
- */
-/*  function calc(a, b) {
-    return (a + b);
- }
-
- console.log(calc(4, 3));
- console.log(calc(5, 3));
- console.log(calc(10, 2));
- console.log(calc(6, 8)); */
-/* 
- function ret() {
-    let num = 50;
-    return num;
- }
-
- const anotherNum = ret();
- console.log(anotherNum);
-
-const logger = function() {
-    console.log('Hello!');
-}
-
-logger();
-
-const calc = (a, b) => {
-    console.log('1');
-    return a + b;
-};
-
-const uahCurs = 3;
-const usdCurs = 38;
-const eurCurs = 45;
-const discount = 0.9;
-
-function converter(amout, Curs) {
-    return Curs * amout;
-}
-
-function promotion(result) {
-    console.log(result * discount);
-}
-
-/* promotion(converter(500, usdCurs));
-promotion(converter(500, uahCurs));
-promotion(converter(500, eurCurs)); */
-
-/*const res = converter(500, usdCurs);
-promotion(res);
-
-function test() {
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        if (i === 3) return 
-    }
-    console.log('Done');
-}
-
-test(); 
-
-
-function sayHello (name) {
-    return `Привіт, ${name}!`;
-}
-
-sayHello('Андрій');
-console.log(sayHello('Андрій'));
-
-
-const str = "test";
-/* const arr = [1, 3, 5];
-
-console.log(str[2]); */
-
-/* console.log(str.toUpperCase());
-
-const fruit = "Some fruit";
-
-console.log(fruit.indexOf("fruit"));
-
-const logg = "Why are you runnig?";
-
-console.log(logg.slice(12, 18));
-
-console.log(logg.substring(12, 18));
-
-console.log(logg.substr(12, 6));
-
-
-const numb = 12.34;
-console.log(Math.round(numb));
-
-for (let i = 5; i <=10; i++) {
-    console.log(i);
-} */ 
-
-
-function learnJS(lang, callback) {
-    console.log(`Я пробою вчити: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log(`Я пройшов урок!!!!`);
-}
-
-learnJS('JavaScript', done);
-
-
-const options = {
-    name: 'test',
-    widht: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log('Test');
-    }
-};
-
-options.makeTest();
-
-const {border, bg} = options.colors; /* - деструктизація */
-console.log(border);
-
-console.log(Object.keys
-    (options).length);   
-
-/* delete options.name; */
-
-let counter = 0;
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Властивість ${i} має значення ${options[key][i]}`);
-            counter++; 
-        }
-    } else {
-       console.log(`Властивість ${key} має значення ${options[key]}`);
-       counter++;  
-    }
-}
-
-console.log(counter);
-
-
-
+div.insertAdjacentHTML('beforebegin','<h2>Hello</h2>');
 
