e3531ffafddbfb93a72b505477b3e4cb32417a8a
AndriyPereginets <115173459+AndriyPereginets@users.noreply.github.com>
2023-02-28 21:36:40 +0200

    MulationObserver and ResizeObserver

--- script.js
+++ script.js
@@ -1,69 +1,13 @@
 'use strict';
 
-/* const box = document.getElementById('box');
+const box = document.querySelector('.box');
 
-console.log(box);
-
-const buttons = document.getElementsByTagName('button')[1];
-
-console.log(buttons);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles [2]);
- // Якщо не звертатись до елементу в доку буде масив, з яким хуй шо зробиш!
-
-const hearts = document.querySelectorAll('.heart');
-
-hearts.forEach(item => {
-    console.log(item);
+let observer = new MutationObserver(MutationRecord => {
+    console.log(MutationRecord);
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart); */
-
-const   box = document.getElementById('box'),
-        buttons = document.getElementsByTagName('button'),
-        circles = document.getElementsByClassName('circle'),
-        hearts = document.querySelectorAll('.heart'),
-        oneHeart = document.querySelector('.heart'),
-        wrapper = document.querySelector('wrapper');
-
-/* box.style.backgroundColor = 'blue';
-box.style.width = '500px'; */
-
-box.style.cssText = 'background-color: blue; width: 500px';
-
-buttons[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-/* for (let i = 0; i < hearts.length; i++) {
-    hearts[i].style.backgroundColor = 'green';
-} */
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
+observer.observe(box, {
+    childList: true
 });
 
-const div = document.createElement('div');
-/* const text = document.createTextNode('Im here!'); */
-div.classList.add('black');
-
- document.body.append(div);
-
-/* document.querySelector('wrapper').append(div); */
-
-/* wrapper.prepend(div); */
-
-/* hearts[0].before(div); */
-/* hearts[0].after(div); */
-
-/* circles[0].remove(); */
-
-/* hearts[0].replaceWith(circles[0]); */
-
-div.innerHTML = '<h1>Hello World!</h1>';
-
-/* div.textContent = 'Hello'; */
-
-div.insertAdjacentHTML('beforebegin','<h2>Hello</h2>');
-
+observer.disconnect();
